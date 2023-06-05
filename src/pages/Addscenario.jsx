6f71df625c32163bee12a836ad177ce36b76db08
Sagar Anumala <sagarpatelanumala7522@gmail.com>
2023-06-05 17:01:37 +0530

    first commit

---
+++ src/pages/Addscenario.jsx
@@ -0,0 +1,67 @@
+import React from 'react'
+import {useRef,useState,useEffect} from 'react'
+import axios from 'axios'
+import "./Addscenario.css"
+import {Link} from "react-router-dom"
+
+
+function Addscenario() {
+     const [data,setData]=useState();
+     const scenarioName=useRef();
+    const scenarioTime=useRef();
+    var rest_api=process.env.REACT_APP_API;
+    
+    const submit=(e)=>{
+            e.preventDefault();
+            var data1={};
+            if(scenarioName.current.value&&scenarioTime.current.value){
+                data1.scenarioName=scenarioName.current.value;
+                data1.scenarioTime=scenarioTime.current.value;
+                data1.vehicleList=[];
+                reset();
+                setData(data1)
+            }else{
+               alert("Please enter both fieds")
+            }
+     }
+     const reset=()=>{
+      scenarioName.current.value='';
+      scenarioTime.current.value='';
+     }
+
+     //ADDING DATA IN API(CREATE)
+     useEffect(()=>{
+      if(data){
+         axios.post(`${rest_api}`,data)
+            .then(res =>{
+               alert('success')
+            }).catch(err => console.log(err));
+        }
+     },[data])
+   
+  return (
+    <div className='container-add'>
+      <span className='add-scenario'>Scenario / add</span>
+      <span className='headding'>Add Scenario</span>
+      <form>
+        <div className='input-fields'>
+           <div>
+              <label htmlFor="scenario_name" style={{color:"white"}}>Scenario Name</label>
+              <input type="text" id="scenario_name" required className='scenario-name1' ref={scenarioName}/>
+           </div>
+           <div>
+              <label htmlFor="scenario_time" style={{color:"white"}}>Scenario Time(seconds)</label>
+              <input type="number" id="scenario_time" required className='scenario-time1' ref={scenarioTime}/>
+           </div>
+         </div>
+        </form>
+        <div className='buttons'>
+           <button type='button' className='submit-btn1' onClick={submit}>Add</button>
+           <button type='button' className='reset-btn1' onClick={reset}>Reset</button>
+           <Link to="/"><button type='button' className='goback-btn1'>Go Back</button></Link>
+        </div>
+    </div>
+  )
+}
+
+export default Addscenario
---
+++ src/pages/Allscenarios.jsx
@@ -0,0 +1,114 @@
+import React from 'react'
+import { useEffect,useState } from 'react'
+import axios from 'axios'
+import './Allscenarios.css'
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+import {Link} from "react-router-dom"
+import Model1 from './Model1';
+import CancelSharpIcon from '@mui/icons-material/CancelSharp';
+import { NavLink } from 'react-router-dom';
+
+function Allscenarios() {
+  const [scenarioData,setScenarioData]=useState([]);    
+  const [scenarioId,setScenarioId]=useState();
+  const [edit,setEdit]=useState(true);                  //TO DISPLAY EDIT MODEL
+  const [editScenario,setEditScenario]=useState({})     //TO PASS  SELECTED SCENARIODATA DATA TO MODEL
+  var rest_api=process.env.REACT_APP_API;               //API URL
+
+  
+  //GETTING SCENARIODATA FROM API
+  const getData=()=>{
+    axios.get(`${rest_api}`)
+          .then(res =>{
+            setScenarioData(res.data)
+          }).catch(err => console.log(err));
+  }
+  useEffect(()=>{
+       getData();
+   },[])
+
+   //DELETING SCENARIO DATA BY ID
+   useEffect(()=>{
+    axios.delete(`${rest_api}${scenarioId}`)
+       .then(res =>{
+         getData();
+       }).catch(err => console.log(err));
+},[scenarioId])
+
+//DELETE SINGLE SCENARIO FUN
+const handleDeleteScenario=(index,e)=>{
+      e.preventDefault();
+      var arr=scenarioData.filter((v,i)=> i==index)
+      console.log(arr[0]._id)
+      setScenarioId(arr[0]._id)
+}
+
+//DELETE ALL SCENARIOS Of API
+const handleDeleteAll=()=>{
+  scenarioData.forEach((item)=>{
+  axios.delete(`${rest_api}${item.id}`)
+          .then(res =>{
+            console.log(res);
+          }).catch(err => console.log(err));
+        })
+    getData();
+}
+
+//EDIT SCENARIO
+const handleEdit= (i,e)=>{
+   setEditScenario(scenarioData[i]);
+    setEdit(!edit);
+}
+const handleCancel=()=>{
+  setEdit(!edit)
+}
+  return (
+    <>
+      {  edit ?
+        <div>
+        <div className='container1'>
+          <div><span className='headding'>All Scenarios</span></div>
+          <div className='buttons'>
+            <Link to="/addscenario"><button type='button' className='add-senario-btn'>New Scenario</button></Link>
+            <Link to="/addvehicle"><button type='button' className='add-vehicle-btn'>Add vehicle</button></Link>
+            <button type='button' className='delete-btn' onClick={handleDeleteAll}>Delete All</button>
+          </div>
+        </div>
+        <table className='table-scenario'>
+            <tr>
+               <th className='scenario-row'>Scenario Id</th>
+               <th className='scenario-row'>Scenario Name</th>
+               <th className='scenario-row'>Scenario Time</th>
+               <th className='scenario-row'>Number of Vehicals</th>
+               <th className='scenario-row'>Add Vehicle</th>
+               <th className='scenario-row'>Edit</th>
+               <th className='scenario-row'>Delete</th>
+            </tr>
+            <tbody>
+                {
+                  scenarioData.map((d,i)=>(
+                    <tr key={i}> 
+                        <td>{i+1}</td>
+                        <td>{d.scenarioName}</td>
+                        <td>{d.scenarioTime}</td>
+                        <td>{d?.vehicleList?.length}</td>
+                        <td><NavLink  to="/addvehicle"><button className='add-vehicle-table-btn'>+</button></NavLink></td>
+                        <td><EditIcon onClick={e=>handleEdit(i,e)}/></td>
+                        <td><DeleteIcon onClick={e=>handleDeleteScenario(i,e)}/></td>
+                    </tr>
+                  ))
+                }
+            </tbody>
+        </table> 
+        </div>:
+        <div className='model-item'>
+          <div className='cancel-icon'><CancelSharpIcon onClick={handleCancel}/>Close</div>
+        <Model1 name={editScenario}/>
+        </div>
+}
+    </>
+  )
+}
+
+export default Allscenarios
